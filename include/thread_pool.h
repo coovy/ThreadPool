e102fc523fc3d8ac37ce2e9dff50d4fd722e6c0a
Your Name <you@example.com>
2024-08-12 22:09:23 +0800

    修改成员变量书写规范，去除build文件夹

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -71,11 +71,11 @@ struct Block {
 class ThreadPool {
  public:
   ThreadPool(size_t count = std::thread::hardware_concurrency(),
-             bool destroy_idle = false)
-      : threads_count(count), destroy_idle(destroy_idle) {
-    threads.resize(THREADS_MAX + 1);
+             bool destroy_idle_ = false)
+      : threads_count_(count), destroy_idle_(destroy_idle_) {
+    threads_.resize(THREADS_MAX_ + 1);
     create_pool(count);
-    manager = std::thread(&ThreadPool::manager_call, this);
+    manager_ = std::thread(&ThreadPool::manager_call, this);
   }
 
   ~ThreadPool() {
@@ -86,7 +86,7 @@ class ThreadPool {
   template <typename F, typename... Args>
   decltype(auto) push(F&& f, Args&&... args) {
     if (is_closed())
-      throw std::runtime_error("Error: Adding tasks on a closed thread pool.");
+      throw std::runtime_error("Error: Adding tasks_ on a closed thread pool.");
     using return_type = std::invoke_result_t<F, Args...>;  //获得F的返回类型
     // std::make_shared创建一个智能指针，指向一个packaged_task对象，
     // packaged_task对象的返回值类型是return_type
@@ -97,11 +97,11 @@ class ThreadPool {
     auto res = task->get_future();
     {
       //锁住任务队列，可以实现自动解锁
-      std::lock_guard<std::mutex> lk(tasks_mtx);
-      tasks.push_back([task]() { (*task)(); });
-      tasks_count++;
+      std::lock_guard<std::mutex> lk(tasks_mtx_);
+      tasks_.push_back([task]() { (*task)(); });
+      tasks_count_++;
     }
-    task_avaliable_cv.notify_one();  //唤醒一个等待的线程
+    task_avaliable_cv_.notify_one();  //唤醒一个等待的线程
     return res;
   }
 
@@ -119,117 +119,121 @@ class ThreadPool {
   }
 
   [[nodiscard]] size_t get_threads_count() const noexcept {
-    return threads_count;
+    return threads_count_;
   }
 
   [[nodiscard]] size_t get_threads_running() const noexcept {
-    return threads_running;
+    return threads_running_;
   }
 
-  [[nodiscard]] size_t get_tasks_count() const noexcept { return tasks_count; }
+  [[nodiscard]] size_t get_tasks_count() const noexcept { return tasks_count_; }
 
   [[nodiscard]] size_t get_tasks_total() const noexcept {
-    return tasks_count + threads_running;
+    return tasks_count_ + threads_running_;
   }
 
   void pause() {
     if (is_paused()) return;
     if (!is_running())
       throw std::runtime_error("Error: Pausing a not-running thread pool.");
-    status = ThreadPoolStatus::PAUSED;
+    status_ = ThreadPoolStatus::PAUSED;
   }
 
   void resume() {  //
     if (is_running()) return;
     if (!is_paused())
       throw std::runtime_error("Error: Resuming a not-paused thread pool.");
-    status = ThreadPoolStatus::RUNNING;
-    task_avaliable_cv.notify_all();
+    status_ = ThreadPoolStatus::RUNNING;
+    task_avaliable_cv_.notify_all();
   }
 
   void clear() {
-    std::lock_guard<std::mutex> lk(tasks_mtx);
-    tasks.clear();
-    tasks_count = 0;
+    std::lock_guard<std::mutex> lk(tasks_mtx_);
+    tasks_.clear();
+    tasks_count_ = 0;
   }
 
   void wait_until_done() {
     if (is_closed() || is_paused()) return;
-    std::unique_lock<std::mutex> lk(tasks_mtx);
-    status = ThreadPoolStatus::WAITING;
+    std::unique_lock<std::mutex> lk(tasks_mtx_);
+    status_ = ThreadPoolStatus::WAITING;
     // wait传入两个参数，1：锁，2：阻塞条件（当它为false时，阻塞）
-    pool_done_cv.wait(lk, [this] { return !threads_running && is_empty(); });
+    pool_done_cv_.wait(lk, [this] { return !threads_running_ && is_empty(); });
   }
 
   template <typename _Rep, typename _Period>
   bool wait_for(const chrono::duration<_Rep, _Period>& _rel_time) {
     if (is_closed() || is_paused()) return true;
-    std::unique_lock<std::mutex> lk(tasks_mtx);
-    status = ThreadPoolStatus::WAITING;
-    bool res = pool_done_cv.wait_for(
-        lk, _rel_time, [this] { return !threads_running && is_empty(); });
+    std::unique_lock<std::mutex> lk(tasks_mtx_);
+    status_ = ThreadPoolStatus::WAITING;
+    bool res = pool_done_cv_.wait_for(
+        lk, _rel_time, [this] { return !threads_running_ && is_empty(); });
     return res;
   }
 
   bool is_running() const noexcept {
-    return status == ThreadPoolStatus::RUNNING;
+    return status_ == ThreadPoolStatus::RUNNING;
   }
 
   bool is_waiting() const noexcept {
-    return status == ThreadPoolStatus::WAITING;
+    return status_ == ThreadPoolStatus::WAITING;
   }
 
-  bool is_paused() const noexcept { return status == ThreadPoolStatus::PAUSED; }
+  bool is_paused() const noexcept {
+    return status_ == ThreadPoolStatus::PAUSED;
+  }
 
-  bool is_closed() const noexcept { return status == ThreadPoolStatus::CLOSED; }
+  bool is_closed() const noexcept {
+    return status_ == ThreadPoolStatus::CLOSED;
+  }
 
-  bool is_empty() const noexcept { return tasks_count == 0; }
+  bool is_empty() const noexcept { return tasks_count_ == 0; }
 
  private:
   void create_pool(size_t count) {
-    status = ThreadPoolStatus::RUNNING;
+    status_ = ThreadPoolStatus::RUNNING;
     for (size_t i = 0; i < count; ++i) {
-      threads[i] = std::thread(&ThreadPool::dispatch, this);
+      threads_[i] = std::thread(&ThreadPool::dispatch, this);
     }
   }
 
   void destroy_pool() {
-    status = ThreadPoolStatus::CLOSED;
-    task_avaliable_cv.notify_all();
-    if (manager.joinable()) manager.join();
-    for (size_t i = 0; i < threads_count; ++i) {
-      if (threads[i].joinable()) threads[i].join();
+    status_ = ThreadPoolStatus::CLOSED;
+    task_avaliable_cv_.notify_all();
+    if (manager_.joinable()) manager_.join();
+    for (size_t i = 0; i < threads_count_; ++i) {
+      if (threads_[i].joinable()) threads_[i].join();
     }
   }
 
   void dispatch() {
     function<void()> task;
     while (true) {
-      std::unique_lock<std::mutex> lk(tasks_mtx);
-      task_avaliable_cv.wait(lk, [this] {
+      std::unique_lock<std::mutex> lk(tasks_mtx_);
+      task_avaliable_cv_.wait(lk, [this] {
         // (非空 || 关闭 || 线程销毁 > 0) && 非暂停  满足时不阻塞，不满足时阻塞
-        return (!is_empty() || is_closed() || threads_destroy > 0) &&
+        return (!is_empty() || is_closed() || threads_destroy_ > 0) &&
                !is_paused();
       });
 
-      if (threads_destroy > 0 && is_empty() && !is_closed()) {
-        --threads_destroy;
+      if (threads_destroy_ > 0 && is_empty() && !is_closed()) {
+        --threads_destroy_;
         ids.emplace(std::this_thread::get_id());
         break;
       }
 
       if (is_closed() && is_empty()) break;
 
-      task = std::move(tasks.front());  // get the first task
-      tasks.pop_front();                // delete the first task
-      --tasks_count;
+      task = std::move(tasks_.front());  // get the first task
+      tasks_.pop_front();                // delete the first task
+      --tasks_count_;
       lk.unlock();
-      ++threads_running;
+      ++threads_running_;
       task();
-      --threads_running;
+      --threads_running_;
 
-      if (is_waiting() && !threads_running && is_empty()) {
-        pool_done_cv.notify_all();
+      if (is_waiting() && !threads_running_ && is_empty()) {
+        pool_done_cv_.notify_all();
       }
     }
   }
@@ -238,100 +242,100 @@ class ThreadPool {
     while (true) {
       if (is_closed()) break;
       // std::this_thread::sleep_for(100ms);
-      if (tasks_count > threads_count && threads_count < THREADS_MAX) {
-        size_t add = min<size_t>(THREADS_ADD, THREADS_MAX - threads_count);
+      if (tasks_count_ > threads_count_ && threads_count_ < THREADS_MAX_) {
+        size_t add = min<size_t>(THREADS_ADD_, THREADS_MAX_ - threads_count_);
         size_t j = 0;
-        std::lock_guard<std::mutex> lk(tasks_mtx);
-        for (size_t i = 0; i < THREADS_MAX && j < add && !ids.empty(); ++i) {
-          if (!threads[i].joinable()) continue;
-          auto id = threads[i].get_id();
+        std::lock_guard<std::mutex> lk(tasks_mtx_);
+        for (size_t i = 0; i < THREADS_MAX_ && j < add && !ids.empty(); ++i) {
+          if (!threads_[i].joinable()) continue;
+          auto id = threads_[i].get_id();
           if (ids.count(id)) {
-            threads[i].join();
-            threads[i] = std::thread(&ThreadPool::dispatch, this);
+            threads_[i].join();
+            threads_[i] = std::thread(&ThreadPool::dispatch, this);
             ids.erase(id);
           }
         }
-        for (size_t i = 0; i < THREADS_MAX && j < add; ++i) {
-          if (!threads[i].joinable()) {
-            threads[i] = std::thread(&ThreadPool::dispatch, this);
+        for (size_t i = 0; i < THREADS_MAX_ && j < add; ++i) {
+          if (!threads_[i].joinable()) {
+            threads_[i] = std::thread(&ThreadPool::dispatch, this);
             ++j;
-            ++threads_count;
+            ++threads_count_;
           }
         }
       }
 
       {
         if (!ids.empty()) {
-          std::lock_guard<std::mutex> lk(tasks_mtx);
-          for (size_t i = 0; i < THREADS_MAX && !ids.empty(); ++i) {
-            if (!threads[i].joinable()) continue;
-            auto id = threads[i].get_id();
+          std::lock_guard<std::mutex> lk(tasks_mtx_);
+          for (size_t i = 0; i < THREADS_MAX_ && !ids.empty(); ++i) {
+            if (!threads_[i].joinable()) continue;
+            auto id = threads_[i].get_id();
             if (ids.count(id)) {
-              threads[i].join();
+              threads_[i].join();
               ids.erase(id);
-              --threads_count;
+              --threads_count_;
             }
           }
         }
       }
 
-      if (destroy_idle) {
-        if (threads_running * 2 < threads_count &&
-            threads_count > THREADS_MIN) {
-          size_t add = min<size_t>(THREADS_ADD, THREADS_MAX - threads_count);
-          this->threads_destroy = add;
+      if (destroy_idle_) {
+        if (threads_running_ * 2 < threads_count_ &&
+            threads_count_ > THREADS_MIN_) {
+          size_t add = min<size_t>(THREADS_ADD_, THREADS_MAX_ - threads_count_);
+          this->threads_destroy_ = add;
         }
       }
     }
   }
 
  private:
-  // A condition variable to notify worker threads that a task is available.
-  condition_variable task_avaliable_cv = {};
+  // A condition variable to notify worker threads_ that a task is available.
+  condition_variable task_avaliable_cv_ = {};
 
-  // A condition variable to notify the main thread that the all tasks have been
-  // done.
-  condition_variable pool_done_cv = {};
+  // A condition variable to notify the main thread that the all tasks_ have
+  // been done.
+  condition_variable pool_done_cv_ = {};
 
-  // A queue of tasks.
-  deque<function<void()>> tasks = {};
+  // A queue of tasks_.
+  deque<function<void()>> tasks_ = {};
 
   // A hash set of thread ids.
   unordered_set<std::thread::id> ids = {};
 
-  // The total number of tasks in queue excluding the running tasks.
-  std::atomic<size_t> tasks_count = 0;
+  // The total number of tasks_ in queue excluding the running tasks_.
+  std::atomic<size_t> tasks_count_ = 0;
 
-  // A mutex to synchronize access to the tasks queue.(同步的)
-  mutex tasks_mtx = {};
+  // A mutex to synchronize access to the tasks_ queue.(同步的)
+  mutex tasks_mtx_ = {};
 
-  // A array of threads.
-  vector<std::thread> threads;
+  // A array of threads_.
+  vector<std::thread> threads_;
 
   // A daemon thread to manage the thread pool.
-  std::thread manager;
+  std::thread manager_;
 
-  // The max number of threads that can be created.
-  static inline size_t THREADS_MAX = 4 * thread::hardware_concurrency();
-  // The min number of threads that can be created.
-  static inline size_t THREADS_MIN =
+  // The max number of threads_ that can be created.
+  static inline size_t THREADS_MAX_ = 4 * thread::hardware_concurrency();
+  // The min number of threads_ that can be created.
+  static inline size_t THREADS_MIN_ =
       min<size_t>(2, thread::hardware_concurrency() / 2);
 
-  // The interval of adding or destroy threads.
-  size_t THREADS_ADD = 4;
+  // The interval of adding or destroy threads_.
+  size_t THREADS_ADD_ = 4;
 
-  // The total number of threads in the pool.
-  std::atomic<size_t> threads_count = 0;
+  // The total number of threads_ in the pool.
+  std::atomic<size_t> threads_count_ = 0;
 
-  // The number of currently running threads.
-  std::atomic<size_t> threads_running = 0;
+  // The number of currently running threads_.
+  std::atomic<size_t> threads_running_ = 0;
 
-  // The number of threads to destroy.
-  std::atomic<size_t> threads_destroy = 0;
+  // The number of threads_ to destroy.
+  std::atomic<size_t> threads_destroy_ = 0;
 
   // A flag of the thread-pool state.
-  std::atomic<ThreadPoolStatus> status = ThreadPoolStatus::CLOSED;
+  std::atomic<ThreadPoolStatus> status_ = ThreadPoolStatus::CLOSED;
 
-  bool destroy_idle = false;
+  bool destroy_idle_ = false;
 };
 }  // namespace tp
